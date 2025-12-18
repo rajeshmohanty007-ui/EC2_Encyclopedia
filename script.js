5d23e581bbdee025498987e8a63d3a6f05846224
rajeshmohanty007-ui <razeshmohanty007@gmail.com>
2025-12-18 19:24:56 +0530

    Pointers added

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-function secReset(){
+function secReset() {
     document.querySelectorAll(".sec").forEach(
         i => {
             i.classList.remove('visible')
         }
     )
     document.querySelectorAll(".btn1").forEach(
-        i =>{
+        i => {
             i.classList.remove('btnX')
         }
     )
@@ -15,45 +15,45 @@ function secReset(){
     //     }
     // )
 }
-function subHtoggle(a){
-     document.getElementsByClassName('subH')[a].classList.toggle('visibleB');
-      document.getElementsByClassName('arrow')[a].classList.toggle('Darrow');
+function subHtoggle(a) {
+    document.getElementsByClassName('subH')[a].classList.toggle('visibleB');
+    document.getElementsByClassName('arrow')[a].classList.toggle('Darrow');
 }
-function home(){
+function home() {
     secReset();
     document.querySelectorAll(".btn1")[0].classList.add('btnX');
     document.querySelectorAll(".sec")[0].classList.add('visible');
 }
-function char(){
+function char() {
     secReset();
     document.querySelectorAll(".btn1")[1].classList.add('btnX');
-      document.querySelectorAll(".sec")[1].classList.add('visible');
-      document.querySelectorAll(".sec")[1].scrollIntoView({
+    document.querySelectorAll(".sec")[1].classList.add('visible');
+    document.querySelectorAll(".sec")[1].scrollIntoView({
         behavior: "smooth",
         block: "start",
         inline: "start"
-      });
+    });
 }
-function map(){
+function map() {
     secReset();
-     document.querySelectorAll(".btn1")[2].classList.add('btnX');
+    document.querySelectorAll(".btn1")[2].classList.add('btnX');
     document.querySelectorAll(".sec")[2].classList.add('visible');
 }
-function items(){
+function items() {
     secReset();
-     document.querySelectorAll(".btn1")[3].classList.add('btnX');
+    document.querySelectorAll(".btn1")[3].classList.add('btnX');
     document.querySelectorAll(".sec")[3].classList.add('visible');
 }
-let homeFunc = ["NPCs","Claris","Chase","Bosses","map()","Edna","Elze","char()"]
+let homeFunc = ["NPCs", "Claris", "Chase", "Bosses", "map()", "Edna", "Elze", "char()"]
 let homeData;
 async function loadData() {
     const response = await fetch("home.json");
     homeData = await response.json();
 }
 loadData().then(
-    ()=>{
+    () => {
         homeData.images.forEach(
-            (e,i) => {
+            (e, i) => {
                 document.getElementById('homeGrid').innerHTML += ` <div class="card1" onclick="${homeFunc[i]}">
                     <img src= ${e} class="img2" alt="">
                     <img src="assets/shade.png" class="img2" alt="">
@@ -62,7 +62,7 @@ loadData().then(
             }
         )
     }
-) 
+)
 let charData;
 async function loadCharData() {
     const response = await fetch("characters.json");
@@ -71,7 +71,7 @@ async function loadCharData() {
 loadCharData().then(
     () => {
         charData.heros.forEach(
-            (e,i) => {
+            (e, i) => {
                 document.getElementById('heros').innerHTML += ` <div class="card2">
                     <img src="${e.img}" class="img3" alt="">
                     <ul class="charData">
@@ -83,7 +83,7 @@ loadCharData().then(
             }
         )
         charData.NPCs.forEach(
-            (e,i) => {
+            (e, i) => {
                 document.getElementById('NPCs').innerHTML += ` <div class="card2">
                     <img src="${e.img}" class="img3" alt="">
                     <ul class="charData charData1">
@@ -96,7 +96,7 @@ loadCharData().then(
             }
         );
         charData.bosses.forEach(
-            (e,i) => {
+            (e, i) => {
                 document.getElementById('bosses').innerHTML += ` <div class="card2">
                     <img src="${e.img}" class="img3" alt="">
                     <ul class="charData charData1">
@@ -109,7 +109,7 @@ loadCharData().then(
             }
         );
         charData.demons.forEach(
-            (e,i) => {
+            (e, i) => {
                 document.getElementById('demons').innerHTML += ` <div class="card2">
                     <img src="${e.img}" class="img3" alt="">
                     <ul class="charData charData1">
@@ -121,7 +121,7 @@ loadCharData().then(
             }
         )
         charData.beasts.forEach(
-            (e,i) => {
+            (e, i) => {
                 document.getElementById('beasts').innerHTML += ` <div class="card2">
                     <img src="${e.img}" class="img3" alt="">
                     <ul class="charData charData1">
@@ -144,7 +144,7 @@ let startX, startY;
 const zoomSpeed = 0.0015;
 const minZoom = 0.5;
 const maxZoom = 4;
-function updatePosition(){
+function updatePosition() {
     FullMap.style.transform = `translate(${PositionX}px,${PositionY}px) scale(${scale})`;
 }
 function clamp(value, min, max) {
@@ -173,43 +173,81 @@ function clampPosition() {
     }
 }
 mapWindow.addEventListener(
-    "wheel", (e) =>{
+    "wheel", (e) => {
         e.preventDefault();
         const rect = mapWindow.getBoundingClientRect();
         const mouseX = e.clientX - rect.left;
         const mouseY = e.clientY - rect.top;
-        const worldX = (mouseX - PositionX)/scale;
-        const worldY = (mouseY - PositionY)/scale;
-        let zoomFactor = 1 - e.deltaY*zoomSpeed;
-        scale = clamp(scale*zoomFactor, minZoom,maxZoom);
-        PositionX = mouseX - worldX*scale;
-        PositionY = mouseY - worldY*scale;
+        const worldX = (mouseX - PositionX) / scale;
+        const worldY = (mouseY - PositionY) / scale;
+        let zoomFactor = 1 - e.deltaY * zoomSpeed;
+        scale = clamp(scale * zoomFactor, minZoom, maxZoom);
+        PositionX = mouseX - worldX * scale;
+        PositionY = mouseY - worldY * scale;
         clampPosition();
         updatePosition();
-    }, {passive: false}
+    }, { passive: false }
 )
 mapWindow.addEventListener(
-    "mousedown", (e) =>{
+    "pointerdown", (e) => {
+        activePointers.set(e.pointerId, e);
+        mapWindow.setPointerCapture(e.pointerId);
+    if(activePointers.size === 1){
         isDragging = true;
-        startX = (e.clientX - PositionX)/scale;
-        startY = (e.clientY - PositionY)/scale;
+        startX = (e.clientX - PositionX) / scale;
+        startY = (e.clientY - PositionY) / scale;
         mapWindow.style.cursor = "grabbing";
         e.preventDefault();
     }
-)
-window.addEventListener(
-    "mouseup", ()=>{
-        isDragging = false;
-        mapWindow.style.cursor = "grab";
     }
 )
+window.addEventListener( "pointerup", endPointer);
+window.addEventListener( "pointercancel", endPointer);
+function endPointer(e){
+     activePointers.delete(e.pointerId);
+     if(activePointers.size < 2) pinchDistance = null;
+       if(activePointers.size === 0)  isDragging = false;
+        mapWindow.style.cursor = "grab";
+        mapWindow.releasePointerCapture(e.pointerId)
+}
 window.addEventListener(
-    "mousemove", (e) =>{
-        if(!isDragging) return;
-        PositionX = e.clientX - startX*scale;
-        PositionY = e.clientY - startY*scale;
+    "pointermove", (e) => {
+        if(!activePointers.has(e.pointerId)) return;
+        activePointers.set(e.pointerId,e);
+        if (activePointers.size === 1 && isDragging){
+        PositionX = e.clientX - startX * scale;
+        PositionY = e.clientY - startY * scale;
+        mapWindow.releasePointerCapture(e.pointerId);
         clampPosition();
         updatePosition();
+        }
+         if (activePointers.size === 2 && isDragging){
+            const [p1,p2] = [...activePointers.values()];
+            const dx = p2.clientX - p1.clientX;
+            const dy = p2.clientY - p1.clientY;
+            const distance = Math.hypot(dx,dy);
+         }
+         if(!pinchDistance){
+            pinchDistance = distance;
+            return
+         }
+         const pinchFactor = distance/pinchDistance;
+         const newScale = clamp(scale*pinchFactor,minZoom,maxZoom);
+         if(newScale === scale) return;
+         const rect = mapWindow.getBoundingClientRect();
+         const centerX = (p1.clientX + p2.clientX)/2 - rect.left;
+         const centerY = (p1.clientY + p2.clientY)/2 - rect.top;
+         const worldX = (centerX - PositionX)/scale;
+         const worldY = (centerY - PositionY)/scale;
+         scale = newScale;
+         PositionX = centerX - worldX*scale;
+         PositionY = centerY - worldY*scale;
+         pinchDistance = distance;
+         clampPosition();
+         updatePosition();
     }
 )
+const activePointers = new Map();
+let pinchDistance = null;
+
 
